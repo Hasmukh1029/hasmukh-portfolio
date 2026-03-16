417c7ea57f47dd55d4350aeb21e4049b57c7b99f
Hasmukh1029 <hasmukh1029@gmail.com>
2026-03-16 23:34:09 +0530

    fix typescript split error

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -12,7 +12,9 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
+
   if (window.innerWidth < 900) return;
+
   const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
   const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
@@ -21,6 +23,7 @@ export default function setSplitText() {
 
   paras.forEach((para: ParaElement) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -31,8 +34,10 @@ export default function setSplitText() {
       linesClass: "split-line",
     });
 
+    if (!para.split) return;
+
     para.anim = gsap.fromTo(
-      para?.split.words,
+      para.split.words || [],
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -48,17 +53,22 @@ export default function setSplitText() {
       }
     );
   });
+
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
+    if (!title.split) return;
+
     title.anim = gsap.fromTo(
-      title?.split.chars,
+      title.split.chars || [],
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
